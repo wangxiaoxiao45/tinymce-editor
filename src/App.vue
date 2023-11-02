e15a66aa6c0eb46f5a1a4159708fd2eabb781c0f
wangxiaoxiao <wxx@tokeninsight.com>
2023-11-02 15:43:50 +0800

    组件库模版

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="text-container">
+      <vue-show-more-text
+        :text="txtString"
+        :lines="3"
+        additional-container-css="margin:14px;"
+        additional-content-css="font-size:16px;"
+        additional-content-expanded-css="font-size:16px;"
+        additional-anchor-css="font-size: 16px;"
+        @click="change"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VueShowMoreText from './components/VueShowMoreText.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
-};
+    VueShowMoreText,
+  },
+  data() {
+    return {
+      txtString: `Note: This property was originally implemented in WebKit and has some issues. It got standardized for legacy support. The CSS Overflow Module Level 3 specification also defines a line-clamp property, which is meant to replace this property and avoid its issues.`,
+    }
+  },
+}
 </script>
 
 <style>
@@ -24,5 +39,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.text-container {
+  width: 200px;
 }
 </style>
